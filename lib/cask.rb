6f7099252cbee8d33aed676d07bca023e01c6141
Will H McMahan <will@thoughtbot.com>
2016-07-09 14:23:05 -0400

    rby

--- lib/cask.rb
+++ lib/cask.rb
@@ -1,4 +1,7 @@
 require "cask/engine"
 
 cask_path = File.expand_path("../../core", __FILE__)
-ENV["SASS_PATH"] = File.join([ENV["SASS_PATH"], cask_path].compact)
+ENV["SASS_PATH"] = [
+  ENV["SASS_PATH"],
+  cask_path,
+].compact.join(File::PATH_SEPARATOR)
